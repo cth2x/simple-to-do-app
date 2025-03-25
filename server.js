219d0a002667912a83883195f799414dba0a3c97
cth2x <colehicks50@gmail.com>
2025-03-25 00:49:23 -0500

    Fixed json + fixed server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Imports
 const express = require('express');
+const path = require('path');
 
 // Creating app instance
 const app = express();
@@ -7,20 +8,23 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '.')));
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 let todos = [];
 
+// POST
 app.post('/todos', (req, res) => {
   const task = req.body;
   todos.push(task);
   res.status(201).send(task);
 });
 
+// PUT
 app.put('/todos/:id', (req, res) => {
   const foundIndex = todos.findIndex(
     (task) => task.id.toString() === req.params.id.toString()
@@ -31,4 +35,40 @@ app.put('/todos/:id', (req, res) => {
   } else {
     res.status(404).send('Task not found');
   }
+});
+
+// GET all tasks
+app.get('/todos', (req, res) => {
+  res.status(200).send(todos);
+});
+
+// GET one task
+app.get('/todos/:id', (req, res) => {
+  const foundIndex = todos.findIndex(
+    (task) => task.id.toString() === req.params.id.toString()
+  );
+  if (foundIndex !== -1) {
+    res.status(200).send(todos[foundIndex]);
+  } else {
+    res.status(404).send('Task not found');
+  }
+});
+
+// DELETE
+app.delete('/todos/:id', (req, res) => {
+  const foundIndex = todos.findIndex(
+    (task) => task.id.toString() === req.params.id.toString()
+  );
+  if (foundIndex !== -1) {
+    const deletedTask = todos[foundIndex];
+    todos.splice(foundIndex, 1);
+    res.status(200).send({ message: 'Task deleted', task: deletedTask });
+  } else {
+    res.status(404).send('Task not found');
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
